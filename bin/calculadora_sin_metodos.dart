0443492afc4c0db70add0f632b45f52161c0b8c2
ApliArte <erbolamm@gmail.com>
2024-10-11 10:17:16 +0200

    inicio

---
+++ bin/calculadora_sin_metodos.dart
@@ -0,0 +1,5 @@
+import 'package:calculadora_sin_metodos/calculadora_sin_metodos.dart' as calculadora_sin_metodos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${calculadora_sin_metodos.calculate()}!');
+}
---
+++ test/calculadora_sin_metodos_test.dart
@@ -0,0 +1,8 @@
+import 'package:calculadora_sin_metodos/calculadora_sin_metodos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
